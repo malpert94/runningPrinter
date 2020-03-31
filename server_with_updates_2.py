13c22e0282923423905485d0edfefb68608e6fed
malpert94 <46949660+malpert94@users.noreply.github.com>
2020-03-31 03:48:20 -0500

    Add files via upload

--- server_with_updates_2.py
+++ server_with_updates_2.py
@@ -9,21 +9,21 @@ from xml.etree.ElementTree import tostring, Element, SubElement
 from threading import Thread, Lock
 from copy import deepcopy
 from datetime import datetime
-import parseFromString1 as pfs
+import parseFromString1 as pfs  # used to parse information from incoming XML
 import logging
-import decArrayProd as dap
-import shift as sr
-import serial
+import decArrayProd as dap  # used to convert incoming commands into decimal array 
+import shift as sr  # used to print 
+import serial  # used to communicate with the Arduino
 # from xmlToVari import xml2vari as xml2v
-logname = datetime.now().strftime("%Y-%m-%d")
-with open(logname + '_PrinterLog.log', 'w'):pass
-logging.basicConfig(filename=logname + '_PrinterLog.log', format='%(asctime)s - %(levelname)s: %(message)s',level=logging.INFO) # 
-logging.info("Program start")
 
-"""Setting up Arduino USB serial port"""
-ard = serial.Serial('/dev/ttyACM0', 9600, timeout = 1)
+logname = datetime.now().strftime("%Y-%m-%d") + '_PrinterLog.log'  # log name based off of date
+with open(logname, 'w'):pass
+logging.basicConfig(filename=logname, format='%(asctime)s - %(levelname)s: %(message)s',level=logging.INFO) # 
+logging.info("Program start")  # logging the start of the program
 
-def sendToArd(signal):
+ard = serial.Serial('/dev/ttyACM0', 9600, timeout = 1)  # Setting up Arduino USB serial port
+
+def sendToArd(signal):  # function to send prime or purge message to Arduino
     ard.write(signal.encode())
     if signal == "1":
         print("[Printer]: Prime sequence initiated")
@@ -32,56 +32,57 @@ def sendToArd(signal):
         print("[Printer]: Purge sequence initiated")
         logging.info("Purge sequence initiated")
 
-def readArd():
+def readArd():  # function to read and decode messages from Arduino
     ret = ard.read().decode("ASCII")#line()#.decode()
     return ret
 
-def prime():
+def prime():  # function to prime system
     global printerActivity
-    sendToArd("1")
-    while ard.inWaiting() < 1:
+    sendToArd("1")  # send a "1" to tell Arduino to prime
+    while ard.inWaiting() < 1:  # wait for response
         continue
-    retu = readArd()
+    retu = readArd()  # read response
     while retu == "1":
-        printerActivity = "Priming"
-        print("Priming...")
+        printerActivity = "Priming"  # check to see if system is priming
+        print("[Printer]: Priming...")
         logging.info("System priming...")
-        while ard.inWaiting() < 1:
+        while ard.inWaiting() < 1:  # wait for response
             continue
         retu = readArd()
-    if retu == "2":
-        print("Primed")
+    if retu == "2":  # check to see if system is primed
+        print("[Printer]: Primed")
         logging.info("System primed and ready")
-        printerActivity = "Ready"
+        printerActivity = "Ready"  # printer ready to start printing
 
-def purge():
+def purge():  # function to purge system
     global printerActivity
-    sendToArd("2")
-    while ard.inWaiting() < 1:
+    sendToArd("2")  # send a "2" to tell Arduino to purge
+    while ard.inWaiting() < 1:  # wait for response
         continue
-    retu = readArd()
+    retu = readArd()  # read response
     while retu == "3":
-        printerActivity = "Purging"
-        print("Purging...")
+        printerActivity = "Purging" # check to see if system is purging
+        print("[Printer]: Purging...")
         logging.info("System purging...")
-        while ard.inWaiting() < 1:
+        while ard.inWaiting() < 1:  # wait for response
             continue
         retu = readArd()
-    if retu == "4":
-        print("Purged")
+    if retu == "4":  # check to see if system is purged
+        print("[Printer]: Purged")
         logging.info("System purged")
         printerActivity = "Purged"
+        
 
 """ Setting up a server to TCP/IP comm testing """
 
-Server_IP = "10.0.0.17"  # home
 # Server_IP = "10.27.11.2"  # school
 # Server_IP = "172.20.10.12" # my iPhone hotspot
+Server_IP = "10.0.0.17"  # home
 Server_Port = 22171
 
 handshake = b"<?xml version='1.0' encoding='UTF-8'?><Blueprint><Handshake><Functional>YES</Functional><Functional>NO</Functional></Handshake></Blueprint>"
 
-class xml2vari():
+class xml2vari():  # set of functions to handle incoming messages
     
     def __init__(self):
         global Text
@@ -108,84 +109,76 @@ class xml2vari():
         Start = None
 
 
-    def parseCmd(message):
+    def parseCmd(message):  # function to parse a command and assign the informatin to variables
         
-        global ID
-        global Text  # update to match stucture set by Rugged Robotics**
-        global Lines
-        global Direction
-        global Length
-        global Begin
-        global job
-        global gap
-        global printerActivity
-        printerActivity = "Processing"
+        global ID  # Command ID
+        global Text  # Text to be printed
+        global Lines  # Lines to be printed
+        global Direction  # Direction for orientation
+        global Length  # length of line(s) to be printed
+        global Begin  # element number to start at (incase of previos error)
+        global job  # decimal array (passed on to printing phase)
+        global gap  # distance between dots (passed on to printing phase)
+        global printerActivity  # current status of printer
+        printerActivity = "Processing"  # set status to processing so no other commands are sent
         
-        contents = message.findall(".//")
-        ID = contents[1].text
+        contents = message.findall(".//")  # create array of information in command than assign each element to a variable
+        ID = contents[1].text 
         Text = contents[2].text
         Lines = contents[3].text
         Direction = contents[4].text
         Length = contents[5].text
         Begin = contents[6].text
         
-        logging.info("Command ID: " + ID)
+        logging.info("Command ID: " + ID)  # log the new command information
         logging.info("Text: " + Text)
         logging.info("Lines: " + Lines)
         logging.info("Direction: " + Direction)
         logging.info("Distance: " + Length)
         logging.info("Begin: " + Begin)
         
-        [job, gap] = dap.create_array(Text, int(Lines), Direction, int(Length))
+        [job, gap] = dap.create_array(Text, int(Lines), Direction, int(Length))  # use the variables to create a decimal array for printing
 #         print(job)
-        Text = None
+        Text = None  # clear out the variables
         Lines = None
         Direction = None
         Length = None
-        printerActivity = "Ready"
+        logging.info("Command ID: " + ID + " ready to print.")
+        printerActivity = "Ready"  # set status to ready ro print
 #         time.sleep(0.003)
-#         params = (Text, Lines, Direction, Length)
-#         return params
 
-    def parseStat(message):
+    def parseStat(message):  # function to parse info from status updates from rover
         
-        global Activity  # update to match stucture set by Rugged Robotics**
-        global Errors
-        global RoverSpeed
-        global Distance
+        global Activity  # is the printer to be active?
+        global Errors  # any errors
+        global RoverSpeed  # speed of rover for printing timing
+        global Distance  # might not need at the moment
         
-        contents = message.findall(".//")
+        contents = message.findall(".//")  # create array of information in command than assign each element to a variable
         Activity = contents[1].text
         Errors = contents[2].text
         RoverSpeed = contents[3].text
         Distance = contents[4].text
-        
-#         try:
-#             logging.info("Activity: " + Activity)
-#             logging.info("Errors: " + Errors)
-#             logging.info("Speed: " + RoverSpeed)
-#             logging.info("Distance: " + Distance)
-#         except TypeError:
-#             pass
-#         params = (Activity, Errors, RoverSpeed, Distance)
-#         return params
 
-    def parseHS(message):
+
+    def parseHS(message):  # function to parse info in Handshake response
         
-        global Functional  # update to match stucture set by Rugged Robotics**
+        global Functional  # Will the printer be in use? (YES or NO)
         global primed
         global printerActivity
-        contents = message.findall(".//")
-        Functional = contents[1].text
+        contents = message.findall(".//")  # create array of inormation in handshake 
+        Functional = contents[1].text  # assign to variable
         print("[Printer]: Handshake received, Functional: ", Functional)
-        logging.info("Functional: " + Functional)
-        if Functional == "YES":
+        logging.info("Functional: " + Functional)  # logg Handshake response
+        if Functional == "YES":  # if the printer is to be used start the priming thread
 #             prime()
 #             primed = 1
             prime_thread = Thread(target=prime)
             prime_thread.daemon = True
             prime_thread.start()
             primed = 1
+        else:
+            sys.exit()  # if printer not to be used shutdown
 #             sendToArd("1")#*********************************************************HERE
 #         while ard.inWaiting() < 1:
 #             continue
@@ -203,47 +196,57 @@ class xml2vari():
 #             printerActivity = "Ready"
             
         
-    def parseTrig(message):
+    def parseTrig(message):  # function to parse trigger
         
-        global Start  # update to match stucture set by Rugged Robotics**
+        global Start  # trigger variable
         
-        contents = message.findall(".//")
-        Start = contents[1].text
+        contents = message.findall(".//")  # create array of inormation in handshake 
+        Start = contents[1].text  # assign info to variable
         logging.info("Start: " + Start)
-        params = (Start)
-        return params
 
-    def checkMessage(message):
-        for elem in message:
-            if elem.tag == "Command":
-                params = xml2vari.parseCmd(message)
-                logging.info("Command ID: " + ID + " ready to print.")
-            elif elem.tag == "Status":
-                params = xml2vari.parseStat(message)
-            elif elem.tag == "Handshake":
-#                 params = xml2vari.parseHS(message)
-                xml2vari.parseHS(message)
-            elif elem.tag == "Trigger":
-                params = xml2vari.parseTrig(message)
-            else:
-                break
-#         return params
 
-    def parseMessage(self, comms):
-        global printerErrors
-        
-        strcomm = comms.decode("ASCII")
-        try:
-            message = ET.XML(comms)
-            params = xml2vari.checkMessage(message)
-            return params
-        except:
-            printerErrors = "1"
-            print("Parsing error")
-            logging.error("Parsing error: " + str(comms))
-            pass
-#         params = xml2vari.checkMessage(message)
-#         return params
+def checkMessage(message):  # function to determine which type of message is to be parsed
+    for elem in message:
+        if elem.tag == "Command":
+            xml2vari.parseCmd(message)                
+        elif elem.tag == "Status":
+            xml2vari.parseStat(message)
+        elif elem.tag == "Handshake":
+            xml2vari.parseHS(message)
+        elif elem.tag == "Trigger":
+            xml2vari.parseTrig(message)
+        else:
+            break
+
+def parseMessage(comms):
+    try:
+        message = ET.XML(comms)
+        checkMessage(message)
+    except:
+#         global printerErrors
+#         printerErrors = "1"
+#         print("Parsing error")
+#         logging.error("Parsing error: " + str(comms))
+        decod = comms.decode("ASCII")
+        ender = "</Rugged>"
+        comd = "<Command>"
+        trigg = "<Trigger>"
+        recovered = []
+        n = decod.count(ender)
+        for x in range(n):
+            t = decod.partition(ender)[0] + decod.partition(ender)[1]
+            decod = decod.partition(ender)[2]
+            if comd in t:
+                recovered.append(t)
+            elif trigg in t:
+                recovered.append(t)
+            elif x == n-1:
+                recovered.append(t)
+        for s in recovered:
+            message = ET.XML(s)
+            checkMessage(message)
+
+
 def sendHB():
     global printerActivity
     global printerErrors
@@ -258,7 +261,10 @@ def sendHB():
         activity.text = printerActivity
         errors.text = printerErrors
         printerHB = tostring(Blueprint)
-        server.connection.sendall(printerHB)
+        try:
+            server.connection.sendall(printerHB)
+        except:
+            break
 #         print(printerActivity)
         printerErrors = "0"
 #         print("printerHB sent")
@@ -299,7 +305,6 @@ class TestServer(object):
         self._data_thread = Thread(target=self._run_data_thread)
         self._data_thread.daemon = True
         self._data_thread.start()
-        input("Send HS: ")##############################################################
         self.connection.sendall(handshake)
         print("[Printer]: Handshake sent to client at %s with port %s" % self.client_address)
         logging.info("Handshake sent to client at %s with port %s" % self.client_address)
@@ -310,7 +315,8 @@ class TestServer(object):
                 if self._data:
 #                     print("Current data in buffer is {}".format(self._data))
                     message = self._data.pop(0)
-                    ser.parseMessage(message)
+#                     ser.
+                    parseMessage(message)
                 else:
                     pass
 #           time.sleep(0.5)
@@ -320,7 +326,7 @@ class TestServer(object):
         while self.connected:
             data = ''
 #             check for data on the buffer
-            ready = select.select([self.connection], [], [], 0.5)
+            ready = select.select([self.connection], [], [], 0.5)######################
 #             if data on the buffer, read it
             if ready[0]:
                 data = self.connection.recv(1024)
@@ -397,7 +403,7 @@ def printing():
     if Activity == "INACTIVE":
         purge()
         logging.info("Printing thread closing")
-        quit()
+        sys.exit()
 
 if __name__ == "__main__":
     global job
@@ -419,11 +425,14 @@ if __name__ == "__main__":
     print_thread.daemon = True
     print_thread.start()
     
+    
     print_thread.join()
+    print("Printing thread closing")
+    sys.exit()
     comm_thread.join()
+    print("comm thread closing")
     status_thread.join()
     
-    quit()
 #     printing()
 
   
